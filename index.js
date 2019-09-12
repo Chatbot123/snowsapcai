b68b6a8af4016a18ac59913be7435d24302e4ecf
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-09-12 10:59:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 5000 
 app.use(bodyParser.json()) 
 
-app.post('/raiseticket', (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body)
 
   res.send({
